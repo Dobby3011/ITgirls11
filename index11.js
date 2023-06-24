66ea474e654e7103e6a8c3b3a73546410ff0966d
Анастасия <doby09@mail.ru>
2023-06-24 18:23:19 +0500

    release v1

---
+++ index11.js
@@ -0,0 +1,35 @@
+const taskInput = document.getElementById('task');
+
+function createTask() {
+    const taskText = taskInput.value;
+    let list = document.querySelector('.list__point');
+    let NewPoint = document.createElement('li');
+    NewPoint.textContent = taskText;
+    list.append(NewPoint);
+    taskInput.value = '';
+}
+
+const tasklist = document.querySelector('ol');
+tasklist.addEventListener('click', function (evt) {
+    if (evt.target.tagName === 'LI') {
+        evt.target.classList.toggle('point');
+    }
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
